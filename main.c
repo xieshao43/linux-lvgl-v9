007fcb34db7bbeb698376e9c812227596a9a6121
xieshao43 <2796534465@qq.com>
2025-03-26 00:51:00 +0800

    third try

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+
+int main(void)
+{
+    lv_init();
+
+    /*Linux frame buffer device init*/
+    lv_display_t * disp = lv_linux_fbdev_create();
+    lv_linux_fbdev_set_file(disp, "/dev/fb0");
+
+    /*Create a Demo*/
+    lv_demo_widgets();
+    lv_demo_widgets_start_slideshow();
+
+    /*Handle LVGL tasks*/
+    while(1) {
+        lv_timer_handler();
+        usleep(5000);
+    }
+
+    return 0;
+}
