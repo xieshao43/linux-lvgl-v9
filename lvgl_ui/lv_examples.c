ed257fa6e18ae5234edfefb3ff1c9ae687f2d02d
xieshao43 <147615094+xieshao43@users.noreply.github.com>
2025-04-05 01:04:44 +0800

    Update lv_examples.c

--- lvgl_ui/lv_examples.c
+++ lvgl_ui/lv_examples.c
@@ -1,15 +1,19 @@
-
 #include "lv_examples.h"
 #include "core/ui_manager.h"
 #include "core/data_manager.h"
 #include "modules/storage_ui.h"
 #include "modules/cpu_ui.h"
+#include "modules/wifi_ui.h"
 #include <stdio.h>
+#include <stdlib.h> 
 
 /**
  * 初始化并显示系统监控UI
  */
 void storage_monitor_init(const char *path) {
+    // 隐藏光标
+    system("echo -e \"\\033[?25l\" > /dev/tty1");
+    
     // 初始化数据管理器
     data_manager_init(path);
 
@@ -21,6 +25,9 @@ void storage_monitor_init(const char *path) {
 
     // 注册CPU模块
     ui_manager_register_module(cpu_ui_get_module());
+    
+    // 注册Wi-Fi模块
+    ui_manager_register_module(wifi_ui_get_module());
 
     // 显示第一个模块
     ui_manager_show_module(0);
